2c0b7ae8f2d8cb0143948eb2826fae3583345895
Idiot <league@WTS14-4.attlocal.net>
2018-11-19 16:54:19 -0800

    unbirthday remarkable ip

---
+++ src/remarkable.java
@@ -0,0 +1,14 @@
+import javax.swing.JOptionPane;
+
+public class remarkable {
+
+	public static void main(String[] args) {
+		String first = JOptionPane.showInputDialog("WHAT IS UR NAME");
+
+		if (first.equals("colin")) {
+			JOptionPane.showMessageDialog(null, "You are good at xolbar!");
+
+		}
+
+	}
+}
